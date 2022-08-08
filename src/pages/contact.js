d16801381d331edfec10225fd52524491ff085bb
loidolt <loidolt@gmail.com>
2022-08-08 15:25:43 -0600

    Use Trigger Email for contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,134 +1,122 @@
-import React from "react";
-import Box from "@mui/material/Box";
+import * as React from 'react';
+import * as Yup from 'yup';
+import { useFormik, Form, FormikProvider } from 'formik';
+import { saveDocumentGenerateID } from '../utils/firestore';
+
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import FormControl from "@mui/material/FormControl";
 import Typography from "@mui/material/Typography";
+import { LoadingButton } from '@mui/lab';
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
-class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      success: "",
-      name: "",
-      email: "",
-      message: "",
-      _gotcha: "",
-      submitting: false,
-    };
-  }
+export default function Contact() {
+  const [open, setOpen] = React.useState(false);
 
-  handleOnChange = (event) => {
-    event.persist();
-    this.setState((prev) => ({
-      ...prev,
-      [event.target.id]: event.target.value,
-    }));
-  };
+  const FormSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email("Please enter a valid email address").required('Email is required'),
+    message: Yup.string().required('Message is required'),
+  });
 
-  render() {
-    return (
-      <Layout>
-        <Seo title="Contact Loidolt Design" />
-        <Typography variant="h3" component="h1" gutterBottom>
-          Contact
-        </Typography>
-        <Paper
-          sx={{
-            padding: 4,
-            backgroundColor: "#1e1e1e",
-            borderRadius: "20px",
-          }}
-        >
-          {this.state.status === "SUCCESS" ? (
-            <Box
-              sx={{
-                padding: 4,
-                color: "#03DAC6",
-              }}
-            >
-              <p>Thanks! I'll get back to you as soon as possible.</p>
-            </Box>
-          ) : (
-            <form name="contact" method="POST" data-netlify="true" >
-              <input type="hidden" name="form-name" value="contact" />
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <FormControl fullWidth variant="filled">
-                    <TextField
-                      id="name"
-                      label="Name"
-                      type="text"
-                      name="name"
-                      variant="outlined"
-                      onChange={this.handleOnChange}
-                    />
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <FormControl fullWidth variant="filled">
-                    <TextField
-                      id="email"
-                      label="Email"
-                      type="email"
-                      name="email"
-                      variant="outlined"
-                      onChange={this.handleOnChange}
-                    />
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControl fullWidth variant="filled">
-                    <TextField
-                      id="message"
-                      label="Message"
-                      name="message"
-                      multiline
-                      rows={5}
-                      variant="outlined"
-                      onChange={this.handleOnChange}
-                    />
-                  </FormControl>
-                </Grid>
-                <input
-                  type="text"
-                  id="_gotcha"
-                  name="_gotcha"
-                  onChange={this.handleOnChange}
-                  style={{ display: "none" }}
-                />
-                <Grid item xs={12}>
-                  <Button
-                    variant="contained"
-                    type="submit"
-                    disabled={this.state.submitting}
-                    sx={{ float: "right" }}
-                  >
-                    SUBMIT
-                  </Button>
-                  {this.state.status === "ERROR" && (
-                    <Box
-                      sx={{
-                        marginTop: 4,
-                        color: "#CF6679",
-                      }}
-                    >
-                      <p>Oops! There was an error. Please try again.</p>
-                    </Box>
-                  )}
-                </Grid>
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      message: '',
+    },
+    validationSchema: FormSchema,
+    onSubmit: async (values, { resetForm }) => {
+      console.log(values)
+      const message = {
+        to: 'loidolt@gmail.com',
+        replyTo: `${values.email}`,
+        message: {
+          subject: `${values.name} | Loidolt Design Contact Form`,
+          text: `${values.message}`,
+          html: `${values.message}`,
+        }
+      }
+      const response = await saveDocumentGenerateID("mail", message)
+      if (response) {
+        resetForm({})
+        setOpen(false)
+      }
+    },
+  });
+
+  const { errors, touched, isSubmitting, values, handleSubmit, getFieldProps, setFieldValue } = formik;
+
+  return (
+    <Layout>
+      <Seo title="Contact Loidolt Design" />
+      <Typography variant="h3" component="h1" gutterBottom>
+        Contact
+      </Typography>
+      <Paper
+        sx={{
+          padding: 4,
+          backgroundColor: "#1e1e1e",
+          borderRadius: "20px",
+        }}
+      >
+        <FormikProvider value={formik}>
+          <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+            <input type="hidden" name="form-name" value="contact" />
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <FormControl fullWidth variant="filled">
+                  <TextField
+                    margin="dense"
+                    id="name"
+                    label="Name"
+                    fullWidth
+                    {...getFieldProps('name')}
+                    error={Boolean(touched.name && errors.name)}
+                    helperText={touched.name && errors.name}
+                  />
+                </FormControl>
               </Grid>
-            </form>
-          )}
-        </Paper>
-      </Layout>
-    );
-  }
-}
-
-export default Contact;
+              <Grid item xs={12} sm={6}>
+                <FormControl fullWidth variant="filled">
+                  <TextField
+                    margin="dense"
+                    id="email"
+                    label="Email"
+                    fullWidth
+                    {...getFieldProps('email')}
+                    error={Boolean(touched.email && errors.email)}
+                    helperText={touched.email && errors.email}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl fullWidth variant="filled">
+                  <TextField
+                    margin="dense"
+                    id="message"
+                    label="Message"
+                    multiline
+                    rows={4}
+                    fullWidth
+                    {...getFieldProps('message')}
+                    error={Boolean(touched.message && errors.message)}
+                    helperText={touched.message && errors.message}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <LoadingButton size="large" type="submit" variant="contained" loading={isSubmitting}>
+                  Submit
+                </LoadingButton>
+              </Grid>
+            </Grid>
+          </Form>
+        </FormikProvider>
+      </Paper>
+    </Layout>
+  );
+};
