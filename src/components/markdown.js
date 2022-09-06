a4a3c3a3f3967883452e66e55cd2c07b4574a0d6
loidolt <loidolt@gmail.com>
2022-09-06 10:45:25 -0600

    Add custom link renderer

--- src/components/markdown.js
+++ src/components/markdown.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import Box from '@mui/material/Box'
 import remarkGfm from 'remark-gfm'
 
-export default function Markdown({ children }) {
+const LinkRenderer = props => {
+    console.log(props)
     return (
-        <ReactMarkdown children={children} remarkPlugins={[remarkGfm]} linkTarget="_blank" />
+        <div>
+            <a
+                href={props.href}
+                target="_blank"
+                rel="noreferrer"
+                style={{
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    whiteSpace: "pre-wrap",
+                    whiteSpace: "-moz-pre-wrap",
+                    whiteSpace: "-pre-wrap",
+                    whiteSpace: "-o-pre-wrap",
+                    wordWrap: "break-word"
+                }}
+            >{props.href}</a>
+        </div >
+    );
+};
+
+export default function Markdown({ children }) {
+    const renderers = {
+        "a": LinkRenderer
+    };
+
+    return (
+        <Box sx={{ width: "100%", position: "relative", }}>
+            <ReactMarkdown children={children} remarkPlugins={[remarkGfm]} components={renderers} linkTarget="_blank" />
+        </Box>
     );
 }
