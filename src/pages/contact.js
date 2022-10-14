7f2eed27037cb9866fd2b1930a14194b5355627a
loidolt <loidolt@gmail.com>
2022-10-14 11:07:57 -0600

    Switch to react-hook-form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,50 +1,56 @@
 import React from 'react';
-import * as Yup from 'yup';
-import { useFormik, Form, FormikProvider } from 'formik';
-
-import { TextField, Paper, Grid, FormControl, Typography } from "@mui/material";
+import { toast } from 'react-toastify';
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useTheme, TextField, Paper, Grid, Typography } from "@mui/material";
 import { LoadingButton } from '@mui/lab';
-import { useTheme } from '@mui/material/styles';
 //firebase
 import { saveDocumentGenerateID } from '../utils/firestore';
 
 import { Layout, Seo } from "../components/layout";
 
+const schema = yup.object().shape({
+  name: yup.string().required('Name is required'),
+  email: yup.string().email("Please enter a valid email address").required('Email is required'),
+  message: yup.string().required('Message is required'),
+});
+
 export default function Contact() {
   const theme = useTheme();
 
-  const FormSchema = Yup.object().shape({
-    name: Yup.string().required('Name is required'),
-    email: Yup.string().email("Please enter a valid email address").required('Email is required'),
-    message: Yup.string().required('Message is required'),
+  const {
+    control,
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting }
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    }
   });
 
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      message: '',
-    },
-    validationSchema: FormSchema,
-    onSubmit: async (values, { resetForm }) => {
-      console.log(values)
-      const message = {
-        to: 'loidolt@gmail.com',
-        replyTo: `${values.email}`,
-        message: {
-          subject: `${values.name} | Loidolt Design Contact Form`,
-          text: `${values.message}`,
-          html: `${values.message}`,
-        }
+  const onSubmit = async (data) => {
+    console.log(data)
+    const message = {
+      to: 'loidolt@gmail.com',
+      replyTo: `${data.email}`,
+      message: {
+        subject: `${data.name} | Loidolt Design Contact Form`,
+        text: `${data.message}`,
+        html: `${data.message}`,
       }
-      const response = await saveDocumentGenerateID("mail", message)
-      if (response) {
-        resetForm({})
-      }
-    },
-  });
-
-  const { errors, touched, isSubmitting, handleSubmit, getFieldProps } = formik;
+    }
+    const response = await saveDocumentGenerateID("mail", message)
+    if (response) {
+      toast.success("Message sent successfully");
+      reset();
+    }
+  };
 
   return (
     <Layout>
@@ -58,59 +64,72 @@ export default function Contact() {
           borderRadius: "20px",
         }}
       >
-        <FormikProvider value={formik}>
-          <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
-            <input type="hidden" name="form-name" value="contact" />
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <FormControl fullWidth variant="filled">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input type="hidden" name="form-name" value="contact" />
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                id="name"
+                name="name"
+                control={control}
+                {...register("name", { required: true })}
+                render={({ field }) =>
                   <TextField
-                    margin="dense"
-                    id="name"
+                    {...field}
                     label="Name"
-                    fullWidth
-                    {...getFieldProps('name')}
-                    error={Boolean(touched.name && errors.name)}
-                    helperText={touched.name && errors.name}
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <FormControl fullWidth variant="filled">
-                  <TextField
                     margin="dense"
-                    id="email"
-                    label="Email"
                     fullWidth
-                    {...getFieldProps('email')}
-                    error={Boolean(touched.email && errors.email)}
-                    helperText={touched.email && errors.email}
+                    error={Boolean(errors.name?.message)}
+                    helperText={errors.name?.message}
                   />
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl fullWidth variant="filled">
-                  <TextField
-                    margin="dense"
-                    id="message"
-                    label="Message"
-                    multiline
-                    rows={4}
-                    fullWidth
-                    {...getFieldProps('message')}
-                    error={Boolean(touched.message && errors.message)}
-                    helperText={touched.message && errors.message}
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <LoadingButton size="large" type="submit" variant="contained" loading={isSubmitting}>
-                  Submit
-                </LoadingButton>
-              </Grid>
+                }
+              />
             </Grid>
-          </Form>
-        </FormikProvider>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                id="email"
+                name="email"
+                control={control}
+                {...register("email", { required: true })}
+                render={({ field }) =>
+                  <TextField
+                    {...field}
+                    label="Email"
+                    margin="dense"
+                    fullWidth
+                    error={Boolean(errors.email?.message)}
+                    helperText={errors.email?.message}
+                  />
+                }
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Controller
+                id="message"
+                name="message"
+                control={control}
+                {...register("message", { required: true })}
+                render={({ field }) =>
+                  <TextField
+                    {...field}
+                    label="Message"
+                    margin="dense"
+                    fullWidth
+                    multiline
+                    rows={6}
+                    error={Boolean(errors.message?.message)}
+                    helperText={errors.message?.message}
+                  />
+                }
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <LoadingButton size="large" type="submit" variant="contained" loading={isSubmitting}>
+                Submit
+              </LoadingButton>
+            </Grid>
+          </Grid>
+        </form>
       </Paper>
     </Layout>
   );
