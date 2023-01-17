b5175d16dae495dbbfe560134b4a37a13681bb2c
loidolt <loidolt@gmail.com>
2023-01-17 12:12:54 -0700

    Fix duplicate key warning

--- src/components/posts/markdown.js
+++ src/components/posts/markdown.js
@@ -1,38 +1,42 @@
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import { Box } from '@mui/material'
-import remarkGfm from 'remark-gfm'
+import { Box } from '@mui/material';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const LinkRenderer = props => {
-    console.log(props)
-    return (
-        <div>
-            <a
-                href={props.href}
-                target="_blank"
-                rel="noreferrer"
-                style={{
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    whiteSpace: "pre-wrap",
-                    whiteSpace: "-moz-pre-wrap",
-                    whiteSpace: "-pre-wrap",
-                    whiteSpace: "-o-pre-wrap",
-                    wordWrap: "break-word"
-                }}
-            >{props.href}</a>
-        </div >
-    );
+  console.log(props);
+  return (
+    <div>
+      <a
+        href={props.href}
+        target="_blank"
+        rel="noreferrer"
+        style={{
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          whiteSpace: 'pre-wrap',
+          wordWrap: 'break-word',
+        }}
+      >
+        {props.href}
+      </a>
+    </div>
+  );
 };
 
 export default function Markdown({ children }) {
-    const renderers = {
-        "a": LinkRenderer
-    };
+  const renderers = {
+    a: LinkRenderer,
+  };
 
-    return (
-        <Box sx={{ width: "100%", position: "relative", }}>
-            <ReactMarkdown children={children} remarkPlugins={[remarkGfm]} components={renderers} linkTarget="_blank" />
-        </Box>
-    );
+  return (
+    <Box sx={{ width: '100%', position: 'relative' }}>
+      <ReactMarkdown
+        children={children}
+        remarkPlugins={[remarkGfm]}
+        components={renderers}
+        linkTarget="_blank"
+      />
+    </Box>
+  );
 }
