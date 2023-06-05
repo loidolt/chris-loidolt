d961d8ef7baba88f96c686638ee6bc5d214c193d
Chris Loidolt <loidolt@gmail.com>
2023-06-05 14:12:56 -0600

    Add basic jest config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
   env: {
     browser: true,
     node: true,
     es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended', 'plugin:jest/all'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   plugins: ['react'],
-  rules: {}
+  rules: {
+    'prefer-const': 'error',
+    'jest/prefer-expect-assertions': 'off',
+  }
 };
